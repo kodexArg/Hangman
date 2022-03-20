cc097d6d07547254f2b9d23f0b53131a318b1d40
KodexArg <gcavedal@gmail.com>
2022-03-20 15:15:13 -0300

    hanguing around - initial commit

---
+++ test.py
@@ -0,0 +1,11 @@
+def run():
+    square = []
+    for i = range(100):
+        v = i**2
+        if v % 3 = 0:
+            square.append(v)
+    print(square)
+
+
+if __name__ == "__main__":
+    run()
